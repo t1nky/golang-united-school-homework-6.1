7c708db61463bc1849ae47291d2d5fb527c2bb6f
alex <haiiryy@gmail.com>
2022-07-14 18:05:20 +0400

    Solution

--- reactangle.go
+++ reactangle.go
@@ -5,3 +5,12 @@ type Rectangle struct {
 	Height, Weight float64
 }
 
+// CalcPerimeter returns calculation result of perimeter
+func (c Rectangle) CalcPerimeter() float64 {
+	return 2 * (c.Height + c.Weight)
+}
+
+// CalcArea returns calculation result of area
+func (c Rectangle) CalcArea() float64 {
+	return c.Weight * c.Height
+}
--- triangle.go
+++ triangle.go
@@ -1,6 +1,18 @@
 package golang_united_school_homework
 
+import "math"
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+// CalcPerimeter returns calculation result of perimeter
+func (c Triangle) CalcPerimeter() float64 {
+	return c.Side * 3
+}
+
+// CalcArea returns calculation result of area
+func (c Triangle) CalcArea() float64 {
+	return math.Sqrt(3) / 4 * math.Pow(c.Side, 2)
 }
